12289d5caeace37877b2d3e95e13b70612b7d775
Venkat Aditya Vellanki <143627172+venkatavellanki@users.noreply.github.com>
2025-07-10 22:21:52 +0530

    Create predict.py

---
+++ src/predict.py
@@ -0,0 +1,19 @@
+# predict.py
+
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+import numpy as np
+
+def generate_caption(model, tokenizer, photo, max_length):
+    in_text = 'startseq'
+    for _ in range(max_length):
+        sequence = tokenizer.texts_to_sequences([in_text])[0]
+        sequence = pad_sequences([sequence], maxlen=max_length)
+        yhat = model.predict([photo, sequence], verbose=0)
+        yhat = np.argmax(yhat)
+        word = tokenizer.index_word.get(yhat)
+        if word is None:
+            break
+        in_text += ' ' + word
+        if word == 'endseq':
+            break
+    return in_text
